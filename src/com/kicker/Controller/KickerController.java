370afd894fdfcc6b8cd089ef6e4c724e3a6b8a61
KickerAdmin <mohisinashaik33@gmail.com>
2015-12-04 12:46:41 +0530

    Update KickerController.java
    Master insert

--- src/com/kicker/Controller/KickerController.java
+++ src/com/kicker/Controller/KickerController.java
@@ -3,8 +3,8 @@ package com.kicker.Controller;
 public class KickerController {
 
 	public static void main(String[] args) {
-		System.out.println("Adding from kicker 2.9");
-System.out.println("adding new line from kicker 2.9");
+		System.out.println("Master insert1");
+System.out.println("Master insert2");
 	System.out.println("inserting from local kicker 2.0");
 	System.out.println("inserting new line in kicker 2.0");
 }
