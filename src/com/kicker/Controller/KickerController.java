c1559eb5434c3c7a366099a9283b6c7cb2e66688
KickerAdmin <mohisinashaik33@gmail.com>
2015-12-04 12:12:43 +0530

    Update KickerController.java
    inserting line 2

--- src/com/kicker/Controller/KickerController.java
+++ src/com/kicker/Controller/KickerController.java
@@ -4,6 +4,7 @@ public class KickerController {
 
 	public static void main(String[] args) {
 		System.out.println("Hello this is kicker 2.0");
+		System.out.println("inserting new line in kicker 2.0");
 	}
 
 }
