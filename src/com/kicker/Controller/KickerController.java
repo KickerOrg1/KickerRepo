786bf42647191fe9a710bcbeeac3d69f3e122d49
KickerAdmin <mohisinashaik33@gmail.com>
2015-12-04 12:09:33 +0530

    Update KickerController.java
    Change from kicker 2.0 branch

--- src/com/kicker/Controller/KickerController.java
+++ src/com/kicker/Controller/KickerController.java
@@ -3,7 +3,7 @@ package com.kicker.Controller;
 public class KickerController {
 
 	public static void main(String[] args) {
-		System.out.println("Hello this is kicker");
+		System.out.println("Hello this is kicker 2.0");
 	}
 
 }
