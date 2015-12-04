fd73f3028d6f902bb3264828f3fa8fcf4f634e57
Shaik <mohisina.shaik@accenture.com>
2015-12-04 12:14:16 +0530

    Second line added from local

--- src/com/kicker/Controller/KickerController.java
+++ src/com/kicker/Controller/KickerController.java
@@ -4,6 +4,7 @@ public class KickerController {
 
 	public static void main(String[] args) {
 		System.out.println("Hello this is kicker 2.0");
+		System.out.println("inserting from local kicker 2.0");
 	}
 
 }
